181b0ec8c22fa3afba0e336f7e921eb3a2ee45dd
benicio <beniciobrandao@hotmail.com>
2025-05-08 08:27:55 -0300

    Cria pasta Repositories para registrar as interfaces

---
+++ Manutec.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Manutec.Core.Entities;
+
+namespace Manutec.Core.Repositories;
+public interface IUserRepository
+{
+    Task<User> Add(User user);
+    Task<List<User>> GetAll();
+    Task<User> GetById(int id);
+    Task<User> Update(User user);
+    Task<User> Delete(int id);
+}

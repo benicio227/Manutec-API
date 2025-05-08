78accf016a91781e0beab12d533fc3d2ce8572d4
benicio <beniciobrandao@hotmail.com>
2025-05-08 00:15:00 -0300

    Mudanças

--- Manutec/Program.cs
+++ Manutec/Program.cs
@@ -1,4 +1,7 @@
 
+using Manutec.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 namespace Manutec;
 
 public class Program
@@ -7,16 +10,18 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        var connectionString = builder.Configuration.GetConnectionString("Manutec.Cs");
+
+        builder.Services.AddDbContext<ManutecDbContext>(o => o.UseSqlServer(connectionString));
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+   
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();

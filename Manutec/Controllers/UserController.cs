ff7605861881593a2d4ef4c24a2bd584290d6b7e
benicio <beniciobrandao@hotmail.com>
2025-05-08 08:29:16 -0300

    Cria pasta para registrar os controllers da aplicação

---
+++ Manutec/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Manutec.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult> Post()
+    {
+        return Created();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Get()
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetById()
+    {
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put()
+    {
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete()
+    {
+        return NoContent();
+    }
+}

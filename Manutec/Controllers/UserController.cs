4581d2a57df69919ab62bef8774cb3650768a259
benicio <beniciobrandao@hotmail.com>
2025-05-10 17:54:37 -0300

    Corrije caminho da rota para boas práticas, adiciona IMediator

--- Manutec/Controllers/UserController.cs
+++ Manutec/Controllers/UserController.cs
@@ -1,37 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Manutec.Application.Commands.UserEntity;
+using Manutec.Application.Queries.UserEntity;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Manutec.Api.Controllers;
-[Route("api/[controller]")]
+[Route("api/workshops/{workShopId}/users")]
 [ApiController]
 public class UserController : ControllerBase
 {
-    [HttpPost]
-    public async Task<ActionResult> Post()
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
     {
-        return Created();
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post(int workShopId, InsertUserCommand command)
+    {
+        command.WorkShopId = workShopId;
+
+        var result = await _mediator.Send(command);
+
+        return Created(string.Empty, result);
     }
 
     [HttpGet]
-    public async Task<ActionResult> Get()
+    public async Task<ActionResult> Get(int workShopId)
     {
-        return Ok();
+        var query = new GetAllUserQuery { WorkShopId = workShopId };
+
+        var result = await _mediator.Send(query);
+
+        return Ok(result);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult> GetById()
+    public async Task<ActionResult> GetById(int workShopId, int id)
     {
-        return Ok();
+
+        var result = await _mediator.Send(new GetByIdUserQuery(workShopId, id));
+
+        return Ok(result);
     }
 
     [HttpPut("{id}")]
-    public async Task<ActionResult> Put()
+    public async Task<ActionResult> Put(int workShopId, int id, UpdateUserCommand command)
     {
+        command.Id = id;
+        command.WorkShopId = workShopId;
+
+        var result = await _mediator.Send(command);
+
         return NoContent();
     }
 
     [HttpDelete("{id}")]
-    public async Task<ActionResult> Delete()
+    public async Task<ActionResult> Delete(int workShopId, int id)
     {
+
+        var result = await _mediator.Send(new DeleteUserCommand(id, workShopId));
+
         return NoContent();
     }
 }

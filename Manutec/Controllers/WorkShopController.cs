797d8ce06105a5068cd5fd75591668f271b585e8
benicio <beniciobrandao@hotmail.com>
2025-05-10 17:42:25 -0300

    Cria Controller para WorkShopController

---
+++ Manutec/Controllers/WorkShopController.cs
@@ -0,0 +1,24 @@
+﻿using Manutec.Application.Commands.WorkShopEntity;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manutec.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class WorkShopController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public WorkShopController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post(InsertWorkShopCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        return Created(string.Empty, result);
+    }
+}

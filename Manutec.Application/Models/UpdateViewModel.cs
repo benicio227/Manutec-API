5e999de812803f5ac747120a189d40c60a743b5a
benicio <beniciobrandao@hotmail.com>
2025-05-10 17:29:27 -0300

    Cria pasta Models para exibir os dados dos modelos

---
+++ Manutec.Application/Models/UpdateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Manutec.Core.Entities;
+
+namespace Manutec.Application.Models;
+public class UpdateViewModel
+{
+    public UpdateViewModel(string userName, string passwordHash, string email, string phone)
+    {
+        UserName = userName;
+        PasswordHash = passwordHash;
+        Email = email;
+        Phone = phone;
+    }
+
+    public string UserName { get; private set; }
+    public string PasswordHash { get; private set; }
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
+
+    public static UpdateViewModel FromEntity(User user)
+    {
+        return new UpdateViewModel(user.UserName, user.PasswordHash, user.Email, user.Phone);
+    }
+}
---
+++ Manutec.Application/Models/VehicleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Manutec.Core.Entities;
+
+namespace Manutec.Application.Models;
+public class VehicleViewModel
+{
+    public VehicleViewModel(string brand, string model, int year, string licensePlate, int currentMileage)
+    {
+        Brand = brand;
+        Model = model;
+        Year = year;
+        LicensePlate = licensePlate;
+        CurrentMileage = currentMileage;
+    }
+
+    public string Brand { get; private set; }
+    public string Model { get; private set; }
+    public int Year { get; private set; }
+    public string LicensePlate { get; private set; }
+    public int CurrentMileage { get; private set; }
+
+    public static VehicleViewModel FromEntity(Vehicle vehicle)
+    {
+        return new VehicleViewModel(vehicle.Brand, vehicle.Model, vehicle.Year, vehicle.LicensePlate, vehicle.CurrentMileage);
+    }
+}

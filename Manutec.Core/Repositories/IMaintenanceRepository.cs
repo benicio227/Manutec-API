7edb2dcafdd682ac76c7bf3f0bd17f77475dde22
benicio <beniciobrandao@hotmail.com>
2025-05-10 17:35:06 -0300

    Cria interface para IMaintenanceRepository

---
+++ Manutec.Core/Repositories/IMaintenanceRepository.cs
@@ -0,0 +1,12 @@
+﻿using Manutec.Core.Entities;
+
+namespace Manutec.Core.Repositories;
+public interface IMaintenanceRepository
+{
+    Task<Maintenance> Add(Maintenance maintenance);
+    Task<List<Maintenance>> GetAllByWorkShopId(int workShopId);
+
+    Task<Maintenance?> GetById(int workShopId, int vehicleId);
+    Task<Maintenance?> Complete(int workShopId, int vehicleId);
+    Task<Maintenance?> Update(Maintenance maintenance);
+}

745411fff566207001c4558ece3c0cfbcb962e6c
benicio <beniciobrandao@hotmail.com>
2025-05-10 17:35:52 -0300

    Cria interface para IVehicleRepository

---
+++ Manutec.Core/Repositories/IVehicleRepository.cs
@@ -0,0 +1,11 @@
+﻿using Manutec.Core.Entities;
+
+namespace Manutec.Core.Repositories;
+public interface IVehicleRepository
+{
+    Task<Vehicle> Add(Vehicle vehicle);
+    Task<List<Vehicle>> GetAllByWorkShopId(int workShopId);
+    Task<Vehicle?> GetById(int workShopid, int customerId, int id);
+    Task<Vehicle?> Update(Vehicle vehicle);
+    Task<Vehicle?> Delete(Vehicle vehicle);
+}

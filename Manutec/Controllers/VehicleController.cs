403926e4f34dca3c424fd6dadf199ce1c0f4a65e
benicio <beniciobrandao@hotmail.com>
2025-05-14 14:31:59 -0300

    Adiciona ILogged

--- Manutec/Controllers/VehicleController.cs
+++ Manutec/Controllers/VehicleController.cs
@@ -2,65 +2,97 @@
 using Manutec.Application.Commands.VehicleEntity;
 using Manutec.Application.Queries.VehicleEntity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Manutec.Infrastructure.Auth;
 
 namespace Manutec.Api.Controllers;
-[Route("api/workshops/{workShopId}/customers/{customerId}/vehicles")]
+[Route("api/customers/{customerId}/vehicles")]
 [ApiController]
+
 public class VehicleController : ControllerBase
 {
     private readonly IMediator _mediator;
-
-    public VehicleController(IMediator mediator)
+    private readonly ILoggedUser _loggedUser;
+    public VehicleController(IMediator mediator, ILoggedUser loggedUser)
     {
         _mediator = mediator;
+        _loggedUser = loggedUser;
     }
 
     [HttpPost]
-    public async Task<ActionResult> Post(int workShopId, int customerId, InsertVehicleCommand command)
+    public async Task<ActionResult> Post(int customerId, InsertVehicleCommand command)
     {
-        command.WorkShopId = workShopId;
+        command.WorkShopId = _loggedUser.WorkShopId;
         command.CustomerId = customerId;
 
         var result = await _mediator.Send(command);
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Message);
+        }
 
         return Created(string.Empty, result);
     }
 
     [HttpGet]
-    public async Task<ActionResult> Get(int workShopId)
+    [Authorize(Roles = "1")]
+    public async Task<ActionResult> Get(int customerId)
     {
-        var query = new GetAllVehicleQuery { WorkShopId = workShopId };
+
+        var query = new GetAllVehicleQuery { WorkShopId = _loggedUser.WorkShopId, CustomerId = customerId };
 
         var result = await _mediator.Send(query);
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Message);
+        }
 
         return Ok(result);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult> GetById(int workShopId, int customerId, int id)
+    [Authorize]
+    public async Task<ActionResult> GetById(int customerId, int id)
     {
 
-        var result = await _mediator.Send(new GetByIdVehicleQuery(id, customerId, workShopId));
+        var result = await _mediator.Send(new GetByIdVehicleQuery(id, customerId, _loggedUser.WorkShopId));
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Message);
+        }
 
         return Ok(result);
     }
 
     [HttpPut("{id}")]
-    public async Task<ActionResult> Put(int workShopId, int customerId, int id, UpdateVehicleCommand command)
+    public async Task<ActionResult> Put(int customerId, int id, UpdateVehicleCommand command)
     {
         command.Id = id;
-        command.WorkShopId = workShopId;
+        command.WorkShopId = _loggedUser.WorkShopId;
         command.CustomerId = customerId;
 
         var result = await _mediator.Send(command);
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Message);
+        }
 
         return NoContent();
     }
 
     [HttpDelete("{id}")]
-    public async Task<ActionResult> Delete(int workShopId, int customerId, int id)
+    public async Task<ActionResult> Delete(int customerId, int id)
     {
-        var result = await _mediator.Send(new DeleteVehicleCommand(id, workShopId, customerId));
+        var result = await _mediator.Send(new DeleteVehicleCommand(id, _loggedUser.WorkShopId, customerId));
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Message);
+        }
 
         return NoContent();
     }

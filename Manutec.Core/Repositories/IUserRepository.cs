c4560328c6b88e3b19ad912b684f513f96509e68
benicio <beniciobrandao@hotmail.com>
2025-05-14 14:28:35 -0300

    Adiciona método para identificar usuario logado

--- Manutec.Core/Repositories/IUserRepository.cs
+++ Manutec.Core/Repositories/IUserRepository.cs
@@ -6,6 +6,7 @@ public interface IUserRepository
     Task<User> Add(User user);
     Task<List<User>> GetAllByWorkShopId(int workShopId);
     Task<User?> GetById(int workShopid, int id);
+    Task<User?> GetByEmailAndPassword(string email, string password);
     Task<User?> Update(User user);
     Task<User?> Delete(User user);
 }

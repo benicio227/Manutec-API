1c3276bbd55d327926a8caca15c79eae620333bd
benicio <beniciobrandao@hotmail.com>
2025-05-14 14:44:27 -0300

    Refatora para retornar ResultViewModel

--- Manutec.Application/Queries/CustomerEntity/GetAllCustomerQuery.cs
+++ Manutec.Application/Queries/CustomerEntity/GetAllCustomerQuery.cs
@@ -1,8 +1,9 @@
-﻿using Manutec.Core.Entities;
+﻿using Manutec.Application.Models;
+using Manutec.Core.Entities;
 using MediatR;
 
 namespace Manutec.Application.Queries.CustomerEntity;
-public class GetAllCustomerQuery : IRequest<List<Customer>>
+public class GetAllCustomerQuery : IRequest<ResultViewModel<List<Customer>>>
 {
     public int WorkShopId {  get; set; }
 }

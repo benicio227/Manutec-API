5600173cee0c205d88a76f3dc32e12414557cd4c
benicio <beniciobrandao@hotmail.com>
2025-05-14 14:47:21 -0300

    Refatora para retonar ResultViewModel

--- Manutec.Application/Queries/UserEntity/GetAllUserHandler.cs
+++ Manutec.Application/Queries/UserEntity/GetAllUserHandler.cs
@@ -4,7 +4,7 @@ using Manutec.Core.Repositories;
 using MediatR;
 
 namespace Manutec.Application.Queries.UserEntity;
-public class GetAllUserHandler : IRequestHandler<GetAllUserQuery, List<User>>
+public class GetAllUserHandler : IRequestHandler<GetAllUserQuery, ResultViewModel<List<User>>>
 {
     private readonly IUserRepository _userRepository;
 
@@ -12,10 +12,10 @@ public class GetAllUserHandler : IRequestHandler<GetAllUserQuery, List<User>>
     {
         _userRepository = userRepository;
     }
-    public async Task<List<User>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
+    public async Task<ResultViewModel<List<User>>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
     {
         var users = await _userRepository.GetAllByWorkShopId(request.WorkShopId);
 
-        return users;
+        return ResultViewModel<List<User>>.Success(users);
     }
 }
--- Manutec.Application/Queries/UserEntity/GetAllUserQuery.cs
+++ Manutec.Application/Queries/UserEntity/GetAllUserQuery.cs
@@ -3,7 +3,7 @@ using Manutec.Core.Entities;
 using MediatR;
 
 namespace Manutec.Application.Queries.UserEntity;
-public class GetAllUserQuery : IRequest<List<User>>
+public class GetAllUserQuery : IRequest<ResultViewModel<List<User>>>
 {
     public int WorkShopId {  get; set; }
 }

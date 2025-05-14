0bee6498d39590d1ff272ac1b578f63d741dc72e
benicio <beniciobrandao@hotmail.com>
2025-05-14 19:29:05 -0300

    Cria pasta WorkShopEntity

---
+++ Manutec.Application/Queries/WorkShopEntity/GetByIdWorkShopHandler.cs
@@ -0,0 +1,26 @@
+﻿using Manutec.Application.Models;
+using Manutec.Application.Models.WorkShopModel;
+using Manutec.Core.Repositories;
+using MediatR;
+
+namespace Manutec.Application.Queries.WorkShopEntity;
+public class GetByIdWorkShopHandler : IRequestHandler<GetByIdWorkShopQuery, ResultViewModel<WorkShopViewModel>>
+{
+    private readonly IWorkShopRepository _workShopRepository;
+
+    public GetByIdWorkShopHandler(IWorkShopRepository workShopRepository)
+    {
+        _workShopRepository = workShopRepository;
+    }
+    public async Task<ResultViewModel<WorkShopViewModel>> Handle(GetByIdWorkShopQuery request, CancellationToken cancellationToken)
+    {
+        var workShop = await _workShopRepository.GetById(request.Id);
+
+        if (workShop is null)
+        {
+            return ResultViewModel<WorkShopViewModel>.Error("Oficina não encontrada.");
+        }
+
+        return ResultViewModel<WorkShopViewModel>.Success(new WorkShopViewModel(workShop.Id));
+    }
+}
---
+++ Manutec.Application/Queries/WorkShopEntity/GetByIdWorkShopQuery.cs
@@ -0,0 +1,10 @@
+﻿using Manutec.Application.Models;
+using Manutec.Application.Models.WorkShopModel;
+using Manutec.Core.Entities;
+using MediatR;
+
+namespace Manutec.Application.Queries.WorkShopEntity;
+public class GetByIdWorkShopQuery : IRequest<ResultViewModel<WorkShopViewModel>>
+{
+    public int Id { get; set; }
+}

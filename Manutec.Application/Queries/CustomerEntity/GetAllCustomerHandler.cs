cc9afb58b1408cbda83067af0e18a71d0fe49a9c
benicio <beniciobrandao@hotmail.com>
2025-05-14 14:22:53 -0300

    Refatora para rertornar RestultViewModel

--- Manutec.Application/Queries/CustomerEntity/GetAllCustomerHandler.cs
+++ Manutec.Application/Queries/CustomerEntity/GetAllCustomerHandler.cs
@@ -1,9 +1,10 @@
-﻿using Manutec.Core.Entities;
+﻿using Manutec.Application.Models;
+using Manutec.Core.Entities;
 using Manutec.Core.Repositories;
 using MediatR;
 
 namespace Manutec.Application.Queries.CustomerEntity;
-public class GetAllCustomerHandler : IRequestHandler<GetAllCustomerQuery, List<Customer>>
+public class GetAllCustomerHandler : IRequestHandler<GetAllCustomerQuery, ResultViewModel<List<Customer>>>
 {
     private readonly ICustomerRepository _customerRepository;
 
@@ -11,10 +12,10 @@ public class GetAllCustomerHandler : IRequestHandler<GetAllCustomerQuery, List<C
     {
         _customerRepository = customerRepository;
     }
-    public async Task<List<Customer>> Handle(GetAllCustomerQuery request, CancellationToken cancellationToken)
+    public async Task<ResultViewModel<List<Customer>>> Handle(GetAllCustomerQuery request, CancellationToken cancellationToken)
     {
         var customers = await _customerRepository.GetAllByWorkShopId(request.WorkShopId);
 
-        return customers;
+        return ResultViewModel<List<Customer>>.Success(customers);
     }
 }

b5ee5e7b7af7d0757bb066d6f4cc119cba1e6185
benicio <beniciobrandao@hotmail.com>
2025-05-14 10:55:24 -0300

    Cria controller para login do usuário

---
+++ Manutec/Controllers/LoginUserController.cs
@@ -0,0 +1,30 @@
+﻿using Manutec.Application.Commands.UserEntity;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manutec.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LoginUserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public LoginUserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpPut("login")]
+    [AllowAnonymous]
+    public async Task<ActionResult> Login([FromBody] LoginUserCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Message);
+        }
+
+        return Ok(result);
+    }
+}

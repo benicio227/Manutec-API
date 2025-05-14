875d4f5aadfcd8ff79dc4b0f1376aa6da259a9c8
benicio <beniciobrandao@hotmail.com>
2025-05-14 10:49:59 -0300

    Cria model de exibição para usuario

---
+++ Manutec.Application/Models/UserModel/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Manutec.Application.Models.UserModel;
+public class LoginViewModel
+{
+    public LoginViewModel(string token)
+    {
+        Token = token;
+    }
+    public string Token {  get; set; }
+}

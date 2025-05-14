c86a666236dcd68bd5385a8f89b73c67e3535f34
benicio <beniciobrandao@hotmail.com>
2025-05-14 10:46:08 -0300

    Cria command para receber informações de login do usuário

---
+++ Manutec.Application/Commands/UserEntity/LoginUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using Manutec.Application.Models;
+using Manutec.Application.Models.UserModel;
+using Manutec.Core.Enums;
+using MediatR;
+
+namespace Manutec.Application.Commands.UserEntity;
+public class LoginUserCommand : IRequest<ResultViewModel<LoginViewModel>>
+{
+    public string Email {  get; set; }
+    public string Password { get; set; }
+}

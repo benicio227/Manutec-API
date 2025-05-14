3ea19b9c187f947a2ab35490d6f44fa1b61dba54
benicio <beniciobrandao@hotmail.com>
2025-05-14 11:07:57 -0300

    Refatora para retornar ResultViewModel

--- Manutec.Application/Commands/MaintenanceEntity/InsertMaintenanceHandler.cs
+++ Manutec.Application/Commands/MaintenanceEntity/InsertMaintenanceHandler.cs
@@ -1,24 +1,47 @@
 ﻿using Manutec.Application.Models;
+using Manutec.Application.Models.MaintenanceModel;
 using Manutec.Core.Repositories;
 using MediatR;
 
 namespace Manutec.Application.Commands.MaintenanceEntity;
-public class InsertMaintenanceHandler : IRequestHandler<InsertMaintenanceCommand, MaintenanceViewModel>
+public class InsertMaintenanceHandler : IRequestHandler<InsertMaintenanceCommand, ResultViewModel<MaintenanceViewModel>>
 {
     private readonly IMaintenanceRepository _maintenanceRepository;
-
-    public InsertMaintenanceHandler(IMaintenanceRepository maintenanceRepository)
+    private readonly IVehicleRepository _vehicleRepository;
+    public InsertMaintenanceHandler(IMaintenanceRepository maintenanceRepository, IVehicleRepository vehicleRepository)
     {
         _maintenanceRepository = maintenanceRepository;
+        _vehicleRepository = vehicleRepository;
     }
-    public async Task<MaintenanceViewModel> Handle(InsertMaintenanceCommand request, CancellationToken cancellationToken)
+    public async Task<ResultViewModel<MaintenanceViewModel>> Handle(InsertMaintenanceCommand request, CancellationToken cancellationToken)
     {
         var maintenance = request.ToEntity();
 
-        await _maintenanceRepository.Add(maintenance);
+        var vheicle = await _vehicleRepository.GetVehicleById(request.VehicleId);
+
+        if (vheicle is null)
+        {
+            return ResultViewModel<MaintenanceViewModel>.Error("Veículo não encontrado.");
+        }
+
+        var diff = maintenance.ScheduledMileage - vheicle.CurrentMileage;
+
+        if (diff <= vheicle.ToleranceKm)
+        {
+            Console.WriteLine($"Manutenção próxima! Diferença: {diff} Km");
+        }
+
+        var maintenanceExist = await _maintenanceRepository.Add(maintenance);
+
+        if (maintenanceExist is null)
+        {
+            return ResultViewModel<MaintenanceViewModel>.Error("Manutenção não cadastrada");
+        }
+
+
 
         var model = MaintenanceViewModel.FromEntity(maintenance);
 
-        return model;
+        return ResultViewModel<MaintenanceViewModel>.Success(model);
     }
 }

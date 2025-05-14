24c2e691606c1cd84589061c6f8d1d463774f838
benicio <beniciobrandao@hotmail.com>
2025-05-14 13:23:40 -0300

    Refatora para retornar ResultViewModel

--- Manutec.Application/Commands/MaintenanceEntity/UpdateMaintenanceStatusCompletedCommand.cs
+++ Manutec.Application/Commands/MaintenanceEntity/UpdateMaintenanceStatusCompletedCommand.cs
@@ -1,8 +1,9 @@
 ﻿using Manutec.Application.Models;
+using Manutec.Application.Models.MaintenanceModel;
 using MediatR;
 
 namespace Manutec.Application.Commands.MaintenanceEntity;
-public class UpdateMaintenanceStatusCompletedCommand : IRequest<UpdateCompletedStatusMaintenanceViewModel>
+public class UpdateMaintenanceStatusCompletedCommand : IRequest<ResultViewModel<UpdateCompletedStatusMaintenanceViewModel>>
 {
     public int Id { get; set; }
     public int WorkShopId {  get; set; }

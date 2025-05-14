0b924fc52760a497286eeee0f360025a83ffcd4c
benicio <beniciobrandao@hotmail.com>
2025-05-14 11:03:09 -0300

    Refatora para retornar ResultViewModel

--- Manutec.Application/Commands/CustomerEntity/InsertCustomerCommand.cs
+++ Manutec.Application/Commands/CustomerEntity/InsertCustomerCommand.cs
@@ -1,9 +1,10 @@
 ﻿using Manutec.Application.Models;
+using Manutec.Application.Models.CustomerModel;
 using Manutec.Core.Entities;
 using MediatR;
 
 namespace Manutec.Application.Commands.CustomerEntity;
-public class InsertCustomerCommand : IRequest<CustomerViewModel>
+public class InsertCustomerCommand : IRequest<ResultViewModel<CustomerViewModel>>
 {
     public string Name { get; set; }
     public string Email { get; set; }

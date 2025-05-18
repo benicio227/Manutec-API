7b16dfa592e7310c3bc5de536774b40069ac272a
benicio <beniciobrandao@hotmail.com>
2025-05-18 12:27:38 -0300

    Cria view model WorkShopVIewModelId

---
+++ Manutec.Application/Models/WorkShopModel/WorkShopViewModelId.cs
@@ -0,0 +1,12 @@
+﻿namespace Manutec.Application.Models.WorkShopModel
+{
+    public class WorkShopViewModelId
+    {
+        public WorkShopViewModelId(int id)
+        {
+            Id = id;
+        }
+
+        public int Id;
+    }
+}

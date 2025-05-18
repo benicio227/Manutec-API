5de45b03c2c061f9ab824e7cfe2b918ce49fc85a
benicio <beniciobrandao@hotmail.com>
2025-05-18 12:13:22 -0300

    Adiciona método FromEntity para retornar a view mode preenchida com dados d entidade e altera construtor com novos valores

--- Manutec.Application/Models/WorkShopModel/WorkShopViewModel.cs
+++ Manutec.Application/Models/WorkShopModel/WorkShopViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace Manutec.Application.Models.WorkShopModel;
+﻿using Manutec.Core.Entities;
+
+namespace Manutec.Application.Models.WorkShopModel;
 public class WorkShopViewModel
 {
-    public WorkShopViewModel(int id)
+    public WorkShopViewModel(string name, string email, string phone)
     {
-        Id = id;
+        Name = name;
+        Email = email;
+        Phone = phone;
     }
-    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
 
+    public static WorkShopViewModel FromEntity(WorkShop workshop)
+    {
+        return new WorkShopViewModel(workshop.Name, workshop.Email, workshop.Phone);
+    }
 }

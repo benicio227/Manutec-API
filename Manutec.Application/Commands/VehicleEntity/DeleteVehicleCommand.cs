2cfcd5b442a378fddbf7c908f908d9b3046d64d3
benicio <beniciobrandao@hotmail.com>
2025-05-18 12:06:15 -0300

    Remove construtor por não ser mais necessário

--- Manutec.Application/Commands/VehicleEntity/DeleteVehicleCommand.cs
+++ Manutec.Application/Commands/VehicleEntity/DeleteVehicleCommand.cs
@@ -4,12 +4,6 @@ using MediatR;
 namespace Manutec.Application.Commands.VehicleEntity;
 public class DeleteVehicleCommand : IRequest<ResultViewModel>
 {
-    public DeleteVehicleCommand(int id, int workShopId, int customerId)
-    {
-        Id = id;
-        WorkShopId = workShopId;
-        CustomerId = customerId;
-    }
     public int Id { get; set; }
     public int WorkShopId { get; set; }
     public int CustomerId { get; set; }

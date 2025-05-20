aff963400216fa5f7aa5452ed226f20178e14214
benicio <beniciobrandao@hotmail.com>
2025-05-20 13:16:30 -0300

    Refatora para retonar uma lista de GetAllUserViewModel ao invés de retornar a entidade

--- Manutec.Application/Queries/UserEntity/GetAllUserHandler.cs
+++ Manutec.Application/Queries/UserEntity/GetAllUserHandler.cs
@@ -1,10 +1,11 @@
 ﻿using Manutec.Application.Models;
+using Manutec.Application.Models.UserModel;
 using Manutec.Core.Entities;
 using Manutec.Core.Repositories;
 using MediatR;
 
 namespace Manutec.Application.Queries.UserEntity;
-public class GetAllUserHandler : IRequestHandler<GetAllUserQuery, ResultViewModel<List<User>>>
+public class GetAllUserHandler : IRequestHandler<GetAllUserQuery, ResultViewModel<List<GetAllUserViewModel>>>
 {
     private readonly IUserRepository _userRepository;
 
@@ -12,10 +13,12 @@ public class GetAllUserHandler : IRequestHandler<GetAllUserQuery, ResultViewMode
     {
         _userRepository = userRepository;
     }
-    public async Task<ResultViewModel<List<User>>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
+    public async Task<ResultViewModel<List<GetAllUserViewModel>>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
     {
         var users = await _userRepository.GetAllByWorkShopId(request.WorkShopId);
 
-        return ResultViewModel<List<User>>.Success(users);
+        var model = GetAllUserViewModel.FromEntity(users);
+
+        return ResultViewModel<List<GetAllUserViewModel>>.Success(model);
     }
 }

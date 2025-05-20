ae988e86380ff137686b154db126e66d7cf86f9e
benicio <beniciobrandao@hotmail.com>
2025-05-20 13:17:20 -0300

    Refatora para retonar uma lista de GetAllUseViewModel ao invés da entidade

--- Manutec.Application/Queries/UserEntity/GetAllUserQuery.cs
+++ Manutec.Application/Queries/UserEntity/GetAllUserQuery.cs
@@ -1,9 +1,10 @@
 ﻿using Manutec.Application.Models;
+using Manutec.Application.Models.UserModel;
 using Manutec.Core.Entities;
 using MediatR;
 
 namespace Manutec.Application.Queries.UserEntity;
-public class GetAllUserQuery : IRequest<ResultViewModel<List<User>>>
+public class GetAllUserQuery : IRequest<ResultViewModel<List<GetAllUserViewModel>>>
 {
     public int WorkShopId {  get; set; }
 }

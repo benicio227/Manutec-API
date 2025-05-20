5964b54204bc75e1712502bd1cf732253dfdc06f
benicio <beniciobrandao@hotmail.com>
2025-05-20 12:56:45 -0300

    Cria arquivo GetAllUserViewModel

---
+++ Manutec.Application/Models/UserModel/GetAllUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Manutec.Core.Entities;
+
+namespace Manutec.Application.Models.UserModel;
+public class GetAllUserViewModel
+{
+    public GetAllUserViewModel(string userName, string email, string phone, DateTime createdAt)
+    {
+        UserName = userName;
+        Email = email;
+        Phone = phone;
+        CreatedAt = createdAt;
+    }
+    public string UserName { get; private set; }
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public static List<GetAllUserViewModel> FromEntity(List<User> users)
+    {
+        return users.Select(user => new GetAllUserViewModel(user.UserName, user.Email, user.Phone, user.CreatedAt)).ToList();
+    }
+}

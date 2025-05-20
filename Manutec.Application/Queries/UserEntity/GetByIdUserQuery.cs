a89ac4912343e294a628a644d3911304d196c326
benicio <beniciobrandao@hotmail.com>
2025-05-20 13:18:52 -0300

    Refatora para retonar tipo GetByIdUserViewmodel ao invés da entidade User

--- Manutec.Application/Queries/UserEntity/GetByIdUserQuery.cs
+++ Manutec.Application/Queries/UserEntity/GetByIdUserQuery.cs
@@ -1,9 +1,10 @@
 ﻿using Manutec.Application.Models;
+using Manutec.Application.Models.UserModel;
 using Manutec.Core.Entities;
 using MediatR;
 
 namespace Manutec.Application.Queries.UserEntity;
-public class GetByIdUserQuery : IRequest<ResultViewModel<User>>
+public class GetByIdUserQuery : IRequest<ResultViewModel<GetByIdUserViewModel>>
 {
     public GetByIdUserQuery(int workShopId, int id)
     {

2658465f8df034c59575adcb2fda88957e439c12
benicio <beniciobrandao@hotmail.com>
2025-05-20 12:57:23 -0300

    Cria arquivo GetByIdUserViewModel

---
+++ Manutec.Application/Models/UserModel/GetByIdUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Manutec.Core.Entities;
+using Manutec.Core.Enums;
+
+namespace Manutec.Application.Models.UserModel;
+public class GetByIdUserViewModel
+{
+    public GetByIdUserViewModel(string userName, string email, string phone, UserRole role, DateTime createdAt)
+    {
+        UserName = userName;
+        Email = email;
+        Phone = phone;
+        Role = role;
+        CreatedAt = createdAt;
+    }
+    public string UserName { get; private set; }
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
+    public UserRole Role { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public static GetByIdUserViewModel FromEntity(User user)
+    {
+        return new GetByIdUserViewModel(user.UserName, user.Email, user.Phone, user.Role, user.CreatedAt);
+    }
+}

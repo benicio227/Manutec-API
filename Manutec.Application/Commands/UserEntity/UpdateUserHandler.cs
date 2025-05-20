eb390ccef7b3baca6a3901983c93dd98c73df653
benicio <beniciobrandao@hotmail.com>
2025-05-20 13:09:51 -0300

    Adiciona criptografia para esconder valor da senha no banco de dados

--- Manutec.Application/Commands/UserEntity/UpdateUserHandler.cs
+++ Manutec.Application/Commands/UserEntity/UpdateUserHandler.cs
@@ -1,16 +1,19 @@
 ﻿using Manutec.Application.Models;
 using Manutec.Application.Models.UserModel;
 using Manutec.Core.Repositories;
+using Manutec.Infrastructure.Auth;
 using MediatR;
 
 namespace Manutec.Application.Commands.UserEntity;
 public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, ResultViewModel<UpdateUserViewModel>>
 {
     private readonly IUserRepository _userRepository;
+    private readonly IAuthService _authService;
 
-    public UpdateUserHandler(IUserRepository userRepository)
+    public UpdateUserHandler(IUserRepository userRepository, IAuthService authService)
     {
         _userRepository = userRepository;
+        _authService = authService;
     }
     public async Task<ResultViewModel<UpdateUserViewModel>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
     {
@@ -18,6 +21,13 @@ public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, ResultViewMo
         var user = request.ToEntity();
 
         var existUser = await _userRepository.GetById(request.WorkShopId, request.Id);
+
+        var passwordHash = _authService.ComputeHash(request.PasswordHash);
+
+        if (passwordHash is null)
+        {
+            return ResultViewModel<UpdateUserViewModel>.Error("Senha não encontrada");
+        }
 
         if (existUser is null)
         {
@@ -27,7 +37,7 @@ public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, ResultViewMo
         existUser.UpdateName(request.UserName);
         existUser.UpdateEmail(request.Email);
         existUser.UpdatePhone(request.Phone);
-        existUser.UpdatePassword(request.PasswordHash);
+        existUser.UpdatePassword(passwordHash);
 
         await _userRepository.Update(existUser);
 

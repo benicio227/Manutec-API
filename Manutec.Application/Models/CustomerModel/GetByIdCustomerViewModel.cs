7b34e76ce411bc73ab509d543dea851722812bf8
benicio <beniciobrandao@hotmail.com>
2025-05-20 12:53:12 -0300

    Cria arquivo GetByIdCustomerViewModel

---
+++ Manutec.Application/Models/CustomerModel/GetByIdCustomerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Manutec.Core.Entities;
+
+namespace Manutec.Application.Models.CustomerModel;
+public class GetByIdCustomerViewModel
+{
+    public GetByIdCustomerViewModel(string name, string email, string phone)
+    {
+        Name = name;
+        Email = email;
+        Phone = phone;
+    }
+
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
+
+    public static GetByIdCustomerViewModel FromEntity(Customer customer)
+    {
+        return new GetByIdCustomerViewModel(customer.Name, customer.Email, customer.Phone);
+    }
+}
